6370f17fe79772ff191b100f8bf26ac0f1f62b01
Arrax Shadowfang <freakyhydra@gmail.com>
2026-09-08 02:31:25 +0200

    Load light theme contrast overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import { App } from './app/App';
 import './styles/index.css';
 import './styles/resizable-panels.css';
 import './styles/diagnostic-turns.css';
+import './styles/light-theme-contrast.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
