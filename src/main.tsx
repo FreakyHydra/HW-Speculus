cf4e507ee274f08d5d7defa4747933e752d7eb42
Arrax Shadowfang <freakyhydra@gmail.com>
2026-09-08 02:02:51 +0200

    Load resizable Speculus panel styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/index.css';
+import './styles/resizable-panels.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
