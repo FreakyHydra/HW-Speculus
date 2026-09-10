038c08bc353734030584d61f0157a3843b36b593
HowlingWhispers <howlingwhispers@proton.me>
2026-09-10 14:03:40 +0200

    Install response calibration control

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import { installComposerExperiment } from './experiments/composer-experiment';
+import { installResponseCalibration } from './experiments/response-calibration';
 import './styles/index.css';
 import './styles/resizable-panels.css';
 import './styles/diagnostic-turns.css';
@@ -11,4 +12,5 @@ import './styles/viewport-layout.css';
 import './styles/composer-experiment.css';
 
 installComposerExperiment();
+installResponseCalibration();
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
