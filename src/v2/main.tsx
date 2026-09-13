b89abd6ad9ae96e0213f232f14accda0a00eab32
HowlingWhispers <howlingwhispers@proton.me>
2026-09-13 07:19:21 +0200

    Load V2 roleplay color styles

--- src/v2/main.tsx
+++ src/v2/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { V2App } from './ui/App';
 import './ui/terminal.css';
+import './ui/roleplay-colors.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><V2App /></StrictMode>);
